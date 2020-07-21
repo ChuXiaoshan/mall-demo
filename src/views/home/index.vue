201fc0219900d6d55e4a8df9a9dfbcd83ea3d325
CxS <xiaoshan1215@gmail.com>
2020-07-21 17:59:50 +0800

    iiii

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,144 @@
 <template>
-    <div>
-        <h1>home</h1>
+  <div class="app-container">
+    <div class="address-layout">
+      <el-row :gutter="20">
+        <el-col :span="6">
+          <div class="out-border">
+            <div class="layout-title">后台项目</div>
+            <div class="color-main address-content">
+              <a href="https://github.com/macrozheng/mall">mall</a>
+            </div>
+          </div>
+        </el-col>
+        <el-col :span="6">
+          <div class="out-border">
+            <div class="layout-title">前端项目</div>
+            <div class="color-main address-content">
+              <a href="https://github.com/macrozheng/mall-admin-web">mall-admin-web</a>
+            </div>
+          </div>
+        </el-col>
+        <el-col :span="6">
+          <div class="out-border">
+            <div class="layout-title">学习教程</div>
+            <div class="color-main address-content">
+              <a href="https://github.com/macrozheng/mall-learning">mall-learning</a>
+            </div>
+          </div>
+        </el-col>
+      </el-row>
     </div>
+    <div class="total-layout">
+      <el-row :gutter="20">
+        <el-col :span="6">
+          <div class="total-frame">
+            <img :src="img_home_order" class="total-icon" />
+            <div class="total-title">今日订单总数</div>
+            <div class="total-value">200</div>
+          </div>
+        </el-col>
+        <el-col :span="6">
+          <div class="total-frame">
+            <img :src="img_home_today_amount" class="total-icon" />
+            <div class="total-title">今日销售总额</div>
+            <div class="total-value">￥8000.00</div>
+          </div>
+        </el-col>
+        <el-col :span="6">
+          <div class="total-frame">
+            <img :src="img_home_yesterday_amount" class="total-icon" />
+            <div class="total-title">昨日销售总额</div>
+            <div class="total-value">￥5111.00</div>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+    <el-card class="mine-layout">
+      <div style="text-align: center">
+        <img width="150px" height="150px" src="http://macro-oss.oss-cn-shenzhen.aliyuncs.com/mall/banner/qrcode_for_macrozheng_258.jpg" />
+      </div>
+      <div style="text-align: center">mall 全套学习教程连载中！</div>
+      <div style="text-align: center; margin-top: 5px"><span class="color-main">关注公众号</span>，第一时间获取</div>
+    </el-card>
+  </div>
 </template>
 
 <script>
+import img_home_order from '@/assets/images/home_order.png'
+import img_home_today_amount from '@/assets/images/home_today_amount.png'
+import img_home_yesterday_amount from '@/assets/images/home_yesterday_amount.png'
 export default {
-    name: 'home'
+  name: 'home',
+  data() {
+    return {
+      img_home_order,
+      img_home_today_amount,
+      img_home_yesterday_amount,
+    }
+  },
 }
 </script>
+
+<style scoped>
+.app-container {
+  margin-top: 40px;
+  margin-left: 120px;
+  margin-right: 120px;
+}
+
+.out-border {
+  border: 1px solid #dcdfe6;
+}
+
+.total-layout {
+  margin-top: 20px;
+}
+
+.total-frame {
+  border: 1px solid #dcdfe6;
+  padding: 20px;
+  height: 100px;
+}
+
+.total-icon {
+  color: #409eff;
+  width: 60px;
+  height: 60px;
+}
+
+.total-title {
+  position: relative;
+  font-size: 16px;
+  color: #909399;
+  left: 70px;
+  top: -50px;
+}
+
+.total-value {
+  position: relative;
+  font-size: 18px;
+  color: #606266;
+  left: 70px;
+  top: -40px;
+}
+
+.layout-title {
+  color: #606266;
+  padding: 15px 20px;
+  background: #f2f6fc;
+  font-weight: bold;
+}
+
+.mine-layout {
+  position: absolute;
+  right: 140px;
+  top: 107px;
+  width: 250px;
+  height: 235px;
+}
+
+.address-content {
+  padding: 20px;
+  font-size: 18px;
+}
+</style>
